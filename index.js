a4a8af3b0440f65cea9848c1a38bbc5c56a45c95
mostafijur1126 <mostafijurrhaman87@gmail.com>
2026-07-29 13:09:46 +0600

    Explor about Precendence of operator

--- index.js
+++ index.js
@@ -65,6 +65,19 @@ const recreateDescription = `${country} is in ${contient} ann it's ${population}
 //   );
 // }
 
+//Precendence of operator
+const now = 2037;
+const ageMostafijur = now - 1991;
+const ageSarah = now - 2018;
+
+// console.log(now - 1991 > now - 2028);
+
+let x, y;
+x = y = 25 - 10 - 5;
+// console.log(x, y);
+
+console.log(10 % 3);
+
 //Type Conversion and Coercion
 // console.log("9" - "5");
 // console.log("19" - "13" + "17");
