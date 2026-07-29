428fc8ced90f0bcc516f156820cdf52bfe67ff31
mostafijur1126 <mostafijurrhaman87@gmail.com>
2026-07-29 09:59:50 +0600

    exploer about equality oparators

--- index.js
+++ index.js
@@ -45,8 +45,27 @@ const recreateDescription = `${country} is in ${contient} ann it's ${population}
 // }
 
 //Type Conversion and Coercion
-console.log("9" - "5");
-console.log("19" - "13" + "17");
-console.log("19" - "13" + 17);
-console.log("123" < 57);
-console.log(5 + 6 + "4" + 9 - 4 - 2);
+// console.log("9" - "5");
+// console.log("19" - "13" + "17");
+// console.log("19" - "13" + 17);
+// console.log("123" < 57);
+// console.log(5 + 6 + "4" + 9 - 4 - 2);
+
+//Equality Operators: == vs. ===
+
+// const numNeighbours = prompt(
+//   "How many neighbour countries does your country have?",
+// );
+
+// LATER : This helps us prevent bugs
+const numNeighbours = Number(
+  prompt("How many neighbour countries does your country have?"),
+);
+
+if (numNeighbours === 1) {
+  console.log("Only 1 border!");
+} else if (numNeighbours > 1) {
+  console.log("More than 1 border");
+} else {
+  console.log("No borders");
+}
